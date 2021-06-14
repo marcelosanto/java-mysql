6abdd2dc76c3a90e27e73817757979d66405f334
Marcelo Santos <rocha.mer21@gmail.com>
2021-06-14 14:07:32 -0300

    criado os fechamentos do banco, da conexao e do resultado

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,12 +1,32 @@
 package program;
 
-import java.sql.Connection;
+import java.sql.*;
+
 
 import db.DB;
 
 public class Main {
     public static void main(String[] args) {
         Connection conn = DB.getConnection();
-        DB.closeConnection(); 
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+        	conn = DB.getConnection();
+        	
+        	st = conn.createStatement();
+        	
+        	rs = st.executeQuery("select * from department");
+        	
+        	while (rs.next()) {
+        		System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+        	}
+        } catch (SQLException e) {
+        	e.printStackTrace();
+        } finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+
     }
 }
